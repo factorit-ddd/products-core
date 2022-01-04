e13eb17adc4ad8efee75444fa5ca5999d313362c
Alex Brouwer <alex@factorit.nl>
2022-01-04 08:09:57 +0000

    open 0.3.x-dev

--- src/Domain/Product.php
+++ src/Domain/Product.php
@@ -5,16 +5,25 @@ namespace Jifeline\Products\Domain;
 
 class Product
 {
-    public static function create(ProductId $id): self {
+    public static function create(ProductId $id): self
+    {
         return new self($id);
     }
 
-    private function __construct(private ProductId $id) {
+    private function __construct(private ProductId $id)
+    {
 
     }
 
     public function id(): ProductId
     {
         return $this->id;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id->toString(),
+        ];
     }
 }
--- src/Domain/ProductId.php
+++ src/Domain/ProductId.php
@@ -3,7 +3,19 @@ declare(strict_types=1);
 
 namespace Jifeline\Products\Domain;
 
-class ProductId
+final class ProductId
 {
+    public static function generate(): self
+    {
+        return new self('abc-'.rand(0, 1000));
+    }
 
+    private function __construct(private string $id)
+    {
+    }
+
+    public function toString(): string
+    {
+        return $this->id;
+    }
 }
